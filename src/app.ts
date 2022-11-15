3fb6f24136501d55203d28d041b870420a4ae7b3
ulianchenko <ulianchenko@gmail.com>
2022-11-15 02:25:17 +0200

    the 2nd lesson - part1

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,49 @@ function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt.innerText = `Hello from ${name}`;
 }
+
+// ===========================================================
+
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular };
+
+type Books = {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+}[];
+
+function getAllBooks(): Books {
+    const books: Books = [
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript,  author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+    return books;
+}
+
+function logFirstAvailable(books: Books): void {
+    console.log(`Number of books: ${books.length}`);
+    const title = books.find(({ available }) => available)?.title;
+    console.log(`First available book: ${title}`);
+}
+
+function getBookTitlesByCategory(inputCategory: Category): string[] {
+    const books = getAllBooks();
+
+    return books
+        .filter(({ category }) => category === inputCategory)
+        .map(({ title }) => title);
+}
+
+function logBookTitles(titles: Array<string>): void {
+    titles.forEach(title => console.log(title));
+}
+
+// ===========================================================
+// Task 02.01
+// console.log(getAllBooks());
+// logFirstAvailable(getAllBooks());
+logBookTitles(getBookTitlesByCategory(Category.CSS));
