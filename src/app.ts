dd348b710ebdc3c8d6e4dc412f0fdb385f43ed6d
ulianchenko <ulianchenko@gmail.com>
2022-12-08 18:12:30 +0200

    the 5th lecture was added

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-redeclare */
+
 showHello('greeting', 'TypeScript');
 
 function showHello(divName: string, name: string) {
@@ -9,31 +11,47 @@ function showHello(divName: string, name: string) {
 
 enum Category { JavaScript, CSS, HTML, TypeScript, Angular };
 
-type Book = {
+// type Book = {
+//     id: number;
+//     title: string;
+//     author: string;
+//     available: boolean;
+//     category: Category;
+// };
+
+interface Book {
     id: number;
     title: string;
     author: string;
     available: boolean;
     category: Category;
+    pages?: number;
+    // markDamaged?: (reason: string) => void;
+    // markDamaged?(reason: string): void;
+    markDamaged?: DamageLogger;
 };
+
+interface DamageLogger {
+    (reason: string): void;
+}
 
 function getAllBooks(): readonly Book[] {
     const books = <const>[
         { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true},
         { id: 2, title: 'JavaScript Testing', category: Category.JavaScript,  author: 'Liang Yuxian Eugene', available: false },
-        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: false },
         { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
     ];
     return books;
 }
 
-function logFirstAvailable(books: readonly Book[]): void {
+function logFirstAvailable(books: readonly Book[] = getAllBooks()): void {
     console.log(`Number of books: ${books.length}`);
     const title = books.find(({ available }) => available)?.title;
     console.log(`First available book: ${title}`);
 }
 
-function getBookTitlesByCategory(inputCategory: Category): string[] {
+function getBookTitlesByCategory(inputCategory: Category = Category.JavaScript): string[] {
     const books = getAllBooks();
 
     return books
@@ -79,6 +97,54 @@ function createCustomer(name: string, age?: number, city?: string): void {
     }
 }
 
+function getBookByID(id: Book['id']): Book | undefined {
+    const books = getAllBooks();
+    return books.find(book => book.id === id);
+}
+
+function checkoutBooks(customer: string, ...bookIDs: number[]): string[] {
+    console.log(`Customer name: ${customer}`);
+    return bookIDs
+        .map(id => getBookByID(id))
+        .filter(book => book.available)
+        .map(book => book.title);
+}
+
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(id: number, available: boolean): string[];
+function getTitles(...args: any): string[] {
+    const books = getAllBooks();
+    if (args.length === 1) {
+        const [arg] = args;
+        if (typeof arg === 'string') {
+            return books.filter(book => book.author === arg).map(book => book.title);
+        } else if (typeof arg === 'boolean') {
+            return books.filter(book => book.available === arg).map(book => book.title);
+        }
+    } else if (args.length === 2) {
+        const [id, available] = args;
+        if (typeof id === 'number' && typeof available === 'boolean') {
+            return books.filter(book => book.id && book.available).map(book => book.title);
+        }
+    }
+}
+
+function assertStringValue(data: any): asserts data is string {
+    if (typeof data !== 'string') {
+        throw new Error('value should have been a string');
+    }
+}
+
+function bookTitleTransform(title: any): string {
+    assertStringValue(title);
+    return [...title].reverse().join('');
+}
+
+function printBook(book: Book): void {
+    console.log(`${book.title} by ${book.author}`);
+}
+
 // ===========================================================
 // Task 02.01
 // console.log(getAllBooks());
@@ -100,6 +166,49 @@ function createCustomer(name: string, age?: number, city?: string): void {
 // console.log(idGenerator('Boris', 20));
 
 // Task 03.02
-createCustomer('Anna');
-createCustomer('Anna', 30);
-createCustomer('Anna', 30, 'Kyiv');
+// createCustomer('Anna');
+// createCustomer('Anna', 30);
+// createCustomer('Anna', 30, 'Kyiv');
+
+// console.log(getBookTitlesByCategory());
+// console.log(getBookTitlesByCategory(Category.CSS));
+
+// logFirstAvailable();
+
+// console.log(getBookByID(1));
+
+// console.log(checkoutBooks('NoName Customer', ...[1, 3, 4]));
+
+// Task 03.03
+// console.log(getTitles(1, true));
+// console.log(getTitles(true));
+// console.log(getTitles(false));
+// console.log(getTitles(2, false));
+// console.log(getTitles('Lea Verou'));
+
+// Task 03.04
+// console.log(bookTitleTransform('Learn TypeScript'));
+// console.log(bookTitleTransform(123));
+// console.log(bookTitleTransform({}));
+
+// Task 04.01
+// const myBook: Book = {
+//     id: 5,
+//     title: 'Colors, Backgrounds, and Grradients',
+//     author: 'Eric A. Meyer',
+//     available: true,
+//     category: Category.CSS,
+//     // year: 2015,
+//     // copies: 3,
+//     pages: 200,
+//     // markDamaged: (reason: string) => console.log(`Damaged: ${reason}`)
+//     markDamaged(reason: string) {
+//         console.log(`Damaged: ${reason}`);
+//     }
+// };
+// printBook(myBook);
+// myBook.markDamaged('missing back cover');
+
+// Task 04.02
+const logDamage: DamageLogger = (reason: string) => console.log(`Damaged: ${reason}`);
+logDamage('missing back cover');
